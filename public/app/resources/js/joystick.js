a70a44ac7a9c282cb94e8be5dabb899ab52889ea
okiloco2 <okiloco2@gmail.com>
2017-11-08 08:54:17 -0500

    Prueba 08-11-2017

--- public/app/resources/js/joystick.js
+++ public/app/resources/js/joystick.js
@@ -1,26 +1,26 @@
 (function($)
 {
 
-        var toggle=false;
+       var toggle=false;
         var down = [];
         $(document).keydown(function(e) {
             down[e.keyCode] = true;
         }).keyup(function(e) {
             if (down[16] && down[78]) {
-                console.log('Nueva Infraccion');
+                alert('Nueva Infraccion');
             }
             if (down[16] && down[67]) {
-                console.log('Captura Imagen');
+                alert('Captura Imagen');
             }
             if (down[16] && down[79]) {
-                console.log('Video de 8 segundos');
+                alert('Video de 8 segundos');
             }
             if (down[16] && down[73]) {
                 if (toggle===false) {
-                    console.log('Iniciar video');
+                    alert('Iniciar video');
                     toggle=true;
                 }else{
-                    console.log('Detener video');
+                    alert('Detener video');
                     toggle=false;
                 }
             }
@@ -34,7 +34,7 @@
         var repGP;  
         var pan_value=0;
         var til_value=-47;
-        var zoom_value=40;
+        var zoom_value=0;
         var pan_continous=0;
 
         function canGame() {
@@ -43,8 +43,9 @@
 
         var once_zoom = function(zoom_value) {
             if(once_zoom.done) return;
-            $.get("http://"+IP_CAMERA+"/axis-cgi/com/ptz.cgi?camera=1&zoom="+zoom_value);
+            $.get("http://"+IP_CAMERA+"/axis-cgi/com/ptz.cgi?camera=1&continuouszoommove=0");
             once_zoom.done = true;
+            zoom_value=0;
             console.log('Reset_Zoom');
         };
 
@@ -66,28 +67,32 @@
                 var j3_pad =2;
                 var j2_pad =1;
                 var j1_pad =0;
-                var movement_dead_zone=0.02;
-                var movement_sensibility = 80;
-                var zoom_dead_zone = 0.4;
-                var zoom_sensibility = 1000;
+                var movement_sensibility = document.getElementById("movementRange").value;
+                var movement_dead_zone = document.getElementById("movemetDZRange").value;
+                var zoom_sensibility = document.getElementById("zoomRange").value;
+                var zoom_dead_zone = document.getElementById("zoomDZRange").value;
+                // var movement_dead_zone=0.02;
+                // var movement_sensibility = 80;
+                // var zoom_dead_zone = 0.4;
+                // var zoom_sensibility = 1000;
 
                 if (gp.buttons[j1_pad].pressed) {
-                    alert('Captura Imagen 1');
+                    console.log('Captura Imagen 1');
                 }
                 if (gp.buttons[j2_pad].pressed) {
-                    alert('Captura Imagen 2');
+                    console.log('Captura Imagen 2');
                 }
                 if (gp.buttons[j3_pad].pressed) {
-                    alert('Captura Imagen 3');
+                    console.log('Captura Imagen 3');
                 }
                 if (gp.buttons[j4_pad].pressed) {
-                    alert('Captura Imagen 4');
+                    console.log('Captura Imagen 4');
                 }
                 if (gp.buttons[left_pad].pressed) {
                     $.get("http://"+IP_CAMERA+"/axis-cgi/com/ptz.cgi?camera=1&move=home&speed=100")
                     pan_value=0;
                     til_value=-47;
-                    zoom_value=40;
+                    zoom_value=0;
                     pan_continous=0;
                     console.log('home');
                 }
@@ -95,7 +100,7 @@
                     $.get("http://"+IP_CAMERA+"/axis-cgi/com/ptz.cgi?camera=1&zoom=40");
                     pan_value=0;
                     til_value=-47;
-                    zoom_value=40;
+                    zoom_value=0;
                     pan_continous=0;
                     console.log('zoom home');
                 }
@@ -103,7 +108,7 @@
                 html+= "Stick "+(Math.ceil(i/2)+1)+": "+gp.axes[i]+","+gp.axes[i+1]+"<br/>";
                 if ((Math.ceil(i/2)+1)===1) {
                         if (Math.abs(gp.axes[i]) > movement_dead_zone) {
-                        pan_value=gp.axes[i]*movement_sensibility / 2;
+                        pan_value=gp.axes[i]*movement_sensibility;
                         if (pan_value >= 180) {
                             pan_value=180;
                         }
@@ -114,14 +119,14 @@
                             pan_continous=0;
                             console.log('Diagonal superior izquierdo');
                             pan_continous=Math.abs(gp.axes[i])*movement_sensibility;
-                        }else if (gp.axes[i] > 0 && gp.axes[i+1] < -0 ) {
+                        }else if (gp.axes[i] > movement_dead_zone && gp.axes[i+1] < -0 ) {
                             pan_continous=0;
                             console.log('Diagonal superior derecho');
                             pan_continous=Math.abs(gp.axes[i])*movement_sensibility;
                         }else if (gp.axes[i] < -movement_dead_zone && gp.axes[i+1] > movement_dead_zone ) {
                             pan_continous=0;
                             console.log('Diagonal inferior izquierdo');
-                            pan_continous=-Math.abs(gp.axes[i]+1)*movement_sensibility;
+                            pan_continous=-Math.abs(gp.axes[i+1])*movement_sensibility;
                         }else if (gp.axes[i] > movement_dead_zone && gp.axes[i+1] > movement_dead_zone ) {
                             pan_continous=0;
                             console.log('Diagonal inferior derecho');
@@ -151,22 +156,24 @@
                  html+= "Stick "+(Math.ceil(i/2)+1)+": "+gp.axes[i]+"<br/>";
                  if ((Math.ceil(i/2)+1)===2) {
                             if (gp.axes[i]>zoom_dead_zone) {
-                                console.log('Zoom ');
-                                    $.get("http://"+IP_CAMERA+"/axis-cgi/com/ptz.cgi?camera=1&zoom=10900");
-                                    zoom_value=zoom_value + gp.axes[i] * 150;
-                                    if (zoom_value >=10900 ) {
-                                        zoom_value=10900;
+                                        zoom_value=zoom_value + gp.axes[i] * zoom_sensibility;
+                                    if (zoom_value >=100 ) {
+                                        zoom_value=99;
                                         }
+                                    console.log('Zoom '+zoom_value);
+                                    $.get("http://"+IP_CAMERA+"/axis-cgi/com/ptz.cgi?camera=1&continuouszoommove="+zoom_value);
                                     once_zoom.done = false;
+                                    zoom_value=0;
                                 
                              }else if (gp.axes[i]<-zoom_dead_zone) {
-                                console.log('Zoom out');
-                                $.get("http://"+IP_CAMERA+"/axis-cgi/com/ptz.cgi?camera=1&zoom=40");
-                                zoom_value=zoom_value - (Math.abs(gp.axes[i]) * 250);
-                                 if (zoom_value <=40 ) {
-                                        zoom_value=40;
+                                zoom_value=zoom_value - Math.abs(gp.axes[i]) * zoom_sensibility;
+                                 if (zoom_value <=-100 ) {
+                                        zoom_value=-99;
                                         }
+                                console.log('Zoom out'+zoom_value);
+                                $.get("http://"+IP_CAMERA+"/axis-cgi/com/ptz.cgi?camera=1&continuouszoommove="+zoom_value);
                                 once_zoom.done = false;
+                                zoom_value=0;
                              }
                              else  {
                                     once_zoom(zoom_value);
@@ -190,7 +197,7 @@
                     hasGP = true;
                     $("#gamepadPrompt").html("Gamepad connected!");
                     console.log("connection event");
-                    repGP = window.setInterval(reportOnGamepad,100);
+                    repGP = window.setInterval(reportOnGamepad,500);
                 });
      
                 $(window).on("gamepaddisconnected", function() {
@@ -205,7 +212,7 @@
                         if(!hasGP) $(window).trigger("gamepadconnected");
                         window.clearInterval(checkGP);
                     }
-                }, 500);
+                }, 100);
             }
      
         });
